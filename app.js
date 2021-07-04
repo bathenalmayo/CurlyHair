2f0b499349bf8de302a252c122182074f0ccb4f5
בת חן אלמיהו <bathenalmayo@gmail.com>
2021-07-04 12:07:32 +0300

    changing package.json

--- app.js
+++ app.js
@@ -146,5 +146,5 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`your on port ${port}`);
+    console.log(`listening on port ${port}`);
 })
